fd6eb43ca5ee64d888578d2669c9ef4cd84a8216
PARSA <parsan.a360@gmail.com>
2023-08-06 22:36:37 +0600

    top bar functionality added

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/assets/logo.svg"
+import logOut from "@/public/assets/logout.svg";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 
 const Topbar = () => {
     return (
@@ -9,6 +11,26 @@ const Topbar = () => {
                 <Image src={logo} alt="logo" width={28} height={28} />
                 <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
             </Link>
+
+            <div className="flex items-center gap-1">
+                <div className="block md:hidden">
+                    <SignedIn>
+                        <SignOutButton>
+                            <div className="flex cursor-pointer">
+                                <Image src={logOut} alt="log out icon" width={24} height={24} />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
+
+                <OrganizationSwitcher
+                    appearance={{
+                        elements: {
+                            organizationSwitcherTrigger: "py-2 px-4"
+                        }
+                    }}
+                />
+            </div>
         </nav>
     )
 }
