476f09d35527bcaa02e1501ba44637b0f038caf5
PARSA <parsan.a360@gmail.com>
2023-08-06 15:22:45 +0600

    Layout structure added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import { UserButton } from "@clerk/nextjs";
+ 
 
  
 export default function Home() {
   return (
-    <main>
-      <UserButton afterSignOutUrl="/" />
-    </main>
+    <>
+      <h1 className="head-text text-left ">This is Home</h1>
+    </>
   )
 }
